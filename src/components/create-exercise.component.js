8307c2134040ef4eb66b4a826ca351db8c4389c2
Mahitha bsl <bslmahitha@gmail.com>
2020-08-11 15:56:00 +0530

    working finr

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -6,9 +7,9 @@ export default class CreateExercise extends Component {
   constructor(props) {
     super(props);
     this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeUsername.bind(this);
-    this.onChangeDuration = this.onChangeUsername.bind(this);
-    this.onChangeDate = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.state = {
       username: '',
@@ -20,9 +21,13 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ['test user'],
-      username: 'test user',
+    axios.get('http://localhost:5000/users').then((response) => {
+      if (response.data.length > 0) {
+        this.setState({
+          users: response.data.map((user) => user.username),
+          username: response.data[0].username,
+        });
+      }
     });
   }
   onChangeUsername(e) {
@@ -55,6 +60,11 @@ export default class CreateExercise extends Component {
       date: this.state.date,
     };
     console.log(exercise);
+
+    axios
+      .post('http://localhost:5000/exercises/add', exercise)
+      .then((res) => console.log(res.data));
+
     window.location = '/';
   }
 
